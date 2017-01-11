61b0f08861a2f1ce5d1198e6ebddb619b3798d0b
Xanible <cbp1222@jagmail.southalabama.edu>
2017-01-11 15:19:32 -0600

    HAMBONE

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,12 @@
 package main;
 
+import x86dictionary.X86Dictionary;
+
 public class Main {
 
 	public static void main(String[] args) {
 		//Create the dictionary
-		
+		X86Dictionary whiteList = new X86Dictionary();
 		//
 		
 		//
--- src/x86dictionary/X86Dictionary.java
+++ src/x86dictionary/X86Dictionary.java
@@ -1,5 +1,8 @@
 package x86dictionary;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class X86Dictionary {
@@ -12,24 +15,44 @@ public class X86Dictionary {
 	//Constructor
 	public X86Dictionary() {
 		//Open dictionary text file
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader("C:/Users/ColbyAdmin/Java-Workspace/Heuristics-Test/OTHER/Dictionary_v3.txt"));
+		} catch (IOException e) {
+			System.out.println("ERROR OPENING DICTIONARY TEXT");
+		}
+		
+		String entry = null;
 		
 		//Loop through text file
 		for(int i = 0;i < 26;i++) {
-			//Read in next line from text file
-			//Set first letter of entry as current first letter
-	
-			boolean changeLetter = false;
-			diction[i] = new ArrayList<String>();
-			
-			//Add entry to current part of dictionary
-			//Begin looping through remaining entries
-			while(!changeLetter) {
-				//Read in next entry
-				//check to see if first letters match
-					//if they do not, set first letter to be changed
-				if(!changeLetter) {
-					//Add entry to current part of dictionary
+			try {
+				// Check if current entry is null
+				if(entry == null || entry.length() == 0) {
+					//If so, Read in next line from text file
+					entry = br.readLine();
 				}
+				
+				//Read in next line from text file
+				
+				//Set first letter of entry as current first letter
+
+				boolean changeLetter = false;
+				diction[i] = new ArrayList<String>();
+				
+				//Add entry to current part of dictionary
+				
+				//Begin looping through remaining entries
+				while(!changeLetter) {
+					//Read in next entry
+					//check to see if first letters match
+						//if they do not, set first letter to be changed
+					if(!changeLetter) {
+						//Add entry to current part of dictionary
+					}
+				}
+			} catch (IOException e) {
+				System.out.println("ERROR READING FROM DICTIONARY TEXT");
 			}
 			
 		}
