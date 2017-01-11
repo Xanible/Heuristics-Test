04f586763545a513f56c532ff94c9d170b23fba9
Xanible <cbp1222@jagmail.southalabama.edu>
2017-01-11 14:35:46 -0600

    Comments added

--- src/x86dictionary/X86Dictionary.java
+++ src/x86dictionary/X86Dictionary.java
@@ -1,5 +1,42 @@
 package x86dictionary;
 
-public class X86Dictionary {
+import java.util.ArrayList;
 
+public class X86Dictionary {
+	
+	//Variables
+	@SuppressWarnings("unchecked")
+	ArrayList<String>[] diction = (ArrayList<String>[])new ArrayList[26];
+	
+	
+	//Constructor
+	public X86Dictionary() {
+		//Open dictionary text file
+		
+		//Loop through text file
+		for(int i = 0;i < 26;i++) {
+			//Read in next line from text file
+			//Set first letter of entry as current first letter
+	
+			boolean changeLetter = false;
+			diction[i] = new ArrayList<String>();
+			
+			//Add entry to current part of dictionary
+			//Begin looping through remaining entries
+			while(!changeLetter) {
+				//Read in next entry
+				//check to see if first letters match
+					//if they do not, set first letter to be changed
+				if(!changeLetter) {
+					//Add entry to current part of dictionary
+				}
+			}
+			
+		}
+		
+	}
+	
+	//Methods
+	
+	
 }
