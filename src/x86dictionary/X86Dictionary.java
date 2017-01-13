5d05021362b9bf3968069120d40ef9430e55b455
Xanible <cbp1222@jagmail.southalabama.edu>
2017-01-13 09:10:32 -0600

    Initialization code for X86Dictionary completed.

--- src/x86dictionary/X86Dictionary.java
+++ src/x86dictionary/X86Dictionary.java
@@ -1,6 +1,7 @@
 package x86dictionary;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -17,32 +18,49 @@ public class X86Dictionary {
 		//Open dictionary text file
 		BufferedReader br = null;
 		try {
-			br = new BufferedReader(new FileReader("C:/Users/ColbyAdmin/Java-Workspace/Heuristics-Test/OTHER/Dictionary_v3.txt"));
+			br = new BufferedReader(new FileReader("C:/Users/Colby/Workspace/Heuristics-Test/OTHER/Dictionary_v3.txt"));
 		} catch (IOException e) {
 			System.out.println("ERROR OPENING DICTIONARY TEXT");
 		}
 		
 		String entry = null;
 		Character first = null;
+		boolean changeLetter = false;
 		
 		//Loop through text file
-		for(int i = 0;i < 26;i++) {
+		for(int i = 0;i < 25;i++) {
 			try {
 				// Check if current entry is null
 				if(entry == null || entry.length() == 0) {
 					//If so, Read in next line from text file
 					entry = br.readLine();
+					first = entry.charAt(0);
+					
+					//Initialize next section
+					diction[i] = new ArrayList<String>();
 				}
-		
-				//Set first letter of entry as current first letter
-				first = entry.charAt(0);
-
-				//Set to loop
-				boolean changeLetter = false;
 				
-				//Initialize next section
-				diction[i] = new ArrayList<String>();
-				System.out.println("SECTION CHANGED");
+				//Set first letter of entry as current first letter
+				while(first != entry.charAt(0)) {
+					
+					//Change first letter to next alphabetically
+					first++;
+					
+					//See if first letters match now
+					if(first == entry.charAt(0)) {
+						//Set to loop
+						changeLetter = false;
+
+						//Initialize next section
+						diction[i] = new ArrayList<String>(); 
+					} else {
+						//Initialize next section
+						diction[i] = new ArrayList<String>();
+						
+						//Increment to next section
+						i++;
+					}
+				}
 				
 				//Add entry to current part of dictionary
 				diction[i].add(entry);
@@ -51,6 +69,18 @@ public class X86Dictionary {
 				while(!changeLetter) {
 					//Read in next entry
 					entry = br.readLine();
+					
+					//Check for end
+					if(entry == null || entry.length() == 0) {
+						//Finish incrementing remaining empty loops
+						for(;i < 25;i++) {
+							first++;
+							
+							//Initialize next section
+							diction[i] = new ArrayList<String>();
+						}
+						break;
+					}
 					
 					//Check to see if first letters match
 					if(first != entry.charAt(0)) {
